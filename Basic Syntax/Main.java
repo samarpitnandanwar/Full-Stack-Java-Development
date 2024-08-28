2249e36fc167beb6018cd7f4834036c8a47fbb1b
SAMARPIT NANDANWAR <161283408+samarpitnandanwar@users.noreply.github.com>
2024-08-28 20:42:59 +0530

    Add files via upload

---
+++ Basic Syntax/Main.java
@@ -0,0 +1,8 @@
+public class Main{   // Class
+    public static void main(String[] args) {   // Method
+        
+
+        System.out.println("Samarpit Nandanwar");   // Code Statement
+    }
+}
+
